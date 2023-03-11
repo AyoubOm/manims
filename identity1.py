51e83c5e23f3a20a59408c8dc848830e47c60b4a
AyoubOm <ayoubomari1@outlook.fr>
2023-03-11 03:39:39 +0100

    working identity nxn-1 = (n-1)(n+1)

--- identity1.py
+++ identity1.py
@@ -10,32 +10,35 @@ class myScene(Scene):
         numberToInclude = [refSquare - i*i for i in range(totalNumbers + 1)]
 
         axes = Axes(
-            x_range=[0, 28],
-            axis_config={
-                "numbers_to_include": numberToInclude,
-                "font_size": FONT_SIZE}
+            x_range=[0, 28]
             )
-
         
-        self.add(axes)
-        self.play(axes.animate)
 
+        number = Text(str(refSquare), font_size=FONT_SIZE)
+        number.move_to([axes.c2p(refSquare, -0.5, 0)])
+
+        self.add(axes)
+        self.play(axes.animate, number.animate)
 
         for i in range(1, totalNumbers+1):
             # Formula
+            number = Text(str(refSquare - i*i), font_size=FONT_SIZE)
+            number.move_to([axes.c2p(refSquare-i*i, -0.5, 0)])
+
             formula = MathTex("{}*{}".format(refNumber-i, refNumber+i), font_size=FONT_SIZE)
             formula.move_to([axes.c2p(refSquare-i*i, 0.5, 0)])
-            self.add(formula)
-            self.play(formula.animate)
+
 
             # Curly Brace
             gap = Brace(Line(start=[0, 0, 0], end=[i*i/2, 0, 0]), sharpness=1).set_color(GREEN_B)
             gap.move_to([axes.c2p(refSquare-i*i/2, -i, 0)])
 
             # Distance number
-            text = Text(str(i*i), font_size=FONT_SIZE)
+            text = Text(str(i*i), font_size=18)
             text.next_to(gap, DOWN)
-            self.add(gap, text)
-            self.play(Create(VGroup(gap, text)))
+            self.add(formula, number, gap, text)
+            self.play(Create(VGroup(formula, number, gap, text)))
+
+            self.wait(0.5)
 
 
